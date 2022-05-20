051241d7d02bd6174e52b29b46a01fb9cf4c057e
rayssalves <rayssa.a.vieira@outlook.com>
2022-05-20 11:06:15 +0200

    only show my space when user is loged in

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -3,15 +3,18 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { selectToken } from "../../store/user/selectors";
+import { selectToken, selectUser } from "../../store/user/selectors";
 import NavbarItem from "./NavbarItem";
 import LoggedIn from "./LoggedIn";
 import LoggedOut from "./LoggedOut";
 
 export default function Navigation() {
   const token = useSelector(selectToken);
-
+  const user = useSelector(selectUser);
+  //only shows my space when user is loged in
   const loginLogoutControls = token ? <LoggedIn /> : <LoggedOut />;
+  const mySpace =
+    token && user ? <NavbarItem path="/other" linkText="My space" /> : false;
 
   return (
     <Navbar bg="light" expand="lg">
@@ -22,7 +25,7 @@ export default function Navigation() {
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav style={{ width: "100%" }} fill>
           <NavbarItem path="/" linkText="spaces" />
-          <NavbarItem path="/other" linkText="My space" />
+          {mySpace}
           {loginLogoutControls}
         </Nav>
       </Navbar.Collapse>
